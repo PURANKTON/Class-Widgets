5d0e1fb376facb8334ca372eaeda98aa09cf4a68
ZhngJah <884256028@qq.com>
2025-04-04 09:49:50 +0800

    修改字体Misans

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ dark_mode_watcher = None
 was_floating_mode = False  # 浮窗状态
 
 if config_center.read_conf('Other', 'do_not_log') != '1':
-    logger.add(f"{base_directory}/log/ClassWidgets_main_{{time}}.log", rotation="1 MB", encoding="utf-8",
+    logger.add(f"{base_directory}/logs/ClassWidgets_main_{{time}}.log", rotation="1 MB", encoding="utf-8",
                retention="1 minute")
     logger.info('未禁用日志输出')
 else:
@@ -1164,7 +1164,7 @@ class openProgressDialog(QWidget):
         backgnd.setGraphicsEffect(shadow_effect)
 
     def init_font(self):
-        font_path = f'{base_directory}/font/HarmonyOS_Sans_SC_Bold.ttf'
+        font_path = f'{base_directory}/font/MiSans-Medium.ttf'
         font_id = QFontDatabase.addApplicationFont(font_path)
         if font_id != -1:
             font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
@@ -1427,7 +1427,7 @@ class FloatingWidget(QWidget):  # 浮窗
         backgnd.setGraphicsEffect(shadow_effect)
 
     def init_font(self):
-        font_path = f'{base_directory}/font/HarmonyOS_Sans_SC_Bold.ttf'
+        font_path = f'{base_directory}/font/MiSans-Medium.ttf'
         font_id = QFontDatabase.addApplicationFont(font_path)
         if font_id != -1:
             font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
@@ -1997,7 +1997,7 @@ class DesktopWidget(QWidget):  # 主要小组件
             self.backgnd.setGraphicsEffect(shadow_effect)
 
     def init_font(self):
-        font_path = f'{base_directory}/font/HarmonyOS_Sans_SC_Bold.ttf'
+        font_path = f'{base_directory}/font/MiSans-Medium.ttf'
         font_id = QFontDatabase.addApplicationFont(font_path)
         if font_id != -1:
             font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
